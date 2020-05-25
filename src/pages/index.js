ba221abd6ed1bbb9d182b44fdcfb366bc386b012
zandazvidrina <zandazvidrina@users.noreply.github.com>
2020-05-25 22:51:54 +1200

    Changed text in Experience section

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,11 +34,11 @@ const IndexPage = () => (
             </div>
           </div>
           <p className="lead mb-5">
-            I am a software developer with 4+ years of industry experience. I am
-            knowledgeable in Angular, CSS, HTML, C# and SQL. I have demonstrated
-            history of working with Selenium WebDriver, Ready!Api and other
-            tools for automated testing. I've finished a Master's Degree focused
-            in IT project management.
+          Software developer with more than 4 years of industry 
+          experience. Knowledgeable in Angular, CSS, HTML, C#, and SQL. 
+          Demonstrated history of working with Selenium WebDriver, HP UFT and 
+          other tools for automated testing. Finished a master’s degree focused
+           on IT project management.
           </p>
           <div className="social-icons">
             {config.socialLinks.map(social => {
@@ -65,38 +65,14 @@ const IndexPage = () => (
             <div className="resume-content">
               <h3 className="mb-0">Full stack developer</h3>
               <div className="subheading mb-3">Trade Me</div>
-              <p>Main responsibilities:</p>
-              <ul>
-                <li>
-                  Be responsible for developing UI pages for Marketplace and
-                  Property using Angular 7/8, HTML5, CSS3, Typescript and SASS
-                </li>
-                <li>
-                  Make Responsive Web Designs and think about accessibility
-                </li>
-                <li>
-                  Write and maintain unit tests using Jest and Marbles
-                  Frameworks
-                </li>
-                <li>
-                  Use RxJS Observables to handle multiple values over time
-                </li>
-                <li>
-                  Make code changes to API and backend using VB, C# and ASP.NET
-                </li>
-                <li>Use JIRA for bug tracking</li>
-                <li>
-                  Use Mercurial for version control
-                </li>
-                <li>
-                  use Jenkins and Teamcity for continuous integration
-                </li>
-                <li>
-                  Use Angular Router to enable navigation from one view to the
-                  next as customer performs application tasks.
-                </li>
-                <li>Use Sentry for error monitoring</li>
-              </ul>
+              <p>
+              Create new features for Trade Me platform using Angular. Make Responsive Web Designs and think about accessibility. 
+              Write and maintain unit tests. Use RxJS Observables to handle multiple values over time. Make code changes to API 
+              and legacy code using VB, C# and ASP.NET. Use Mercurial for version control. Use Angular Router to enable navigation from one view to the next. Monitor errors in Sentry. 
+	              </p>
+                <p>
+                <strong>Tech stack: Angular ♦ Typescript ♦ HTML ♦ CSS ♦ JavaScript ♦ Sass ♦ RxJs ♦ NgRx ♦ Jest ♦ Marbles ♦ C# ♦ VB.NET ♦ ASP.NET ♦ WebAPI ♦ SQL Server ♦ Mercurial ♦ Scrum</strong>
+                </p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">October 2019 - Present</span>
@@ -106,44 +82,18 @@ const IndexPage = () => (
             <div className="resume-content">
               <h3 className="mb-0">Test Analyst</h3>
               <div className="subheading mb-3">Trade Me</div>
-              <p>Main responsibilities:</p>
-              <ul>
-                <li>
-                  Plan and execute manual testing for projects including
-                  functional, regression, API testing
-                </li>
-                <li>
-                  Design and implement Automation frameworks like Page Object
-                  model framework for Selenium Webdriver UI automation using
-                  XPATH and CSS selectors
-                </li>
-                <li>
-                  Setup environment from scratch for API testing in Ready!API
-                  for my project
-                </li>
-                <li>
-                  Create and maintain API automation tests using Ready!API and
-                  Gherkin
-                </li>
-                <li>
-                  Create and maintain UI automation tests using Selenium
-                  WebDriver with C#, .NET framework, xunit, Specflow
-                </li>
-                <li>Use Mercurial for code base versioning and branching</li>
-                <li>Configure and use Jenkins for continuous integration</li>
-                <li>
-                  Be responsible for deployments to production and checking
-                  errors in Splunk
-                </li>
-                <li>
-                  Use T-SQL for database queries and write scripts for database
-                  refreshes to help and support testing
-                </li>
-                <li>
-                  Be an active member for planning future projects, priority of
-                  tasks
-                </li>
-              </ul>
+              <p>
+              Manage testing practices for an accommodation service provider. 
+              Setup test automation environments from scratch using Selenium Webdriver and ReadyAPI. 
+              Plan and execute manual testing for projects including functional, regression, API testing. 
+              Create and maintain API and UI automation tests. Use Mercurial for code base versioning and branching. 
+              Configure and use Jenkins for continuous integration for UI automation. Deploy to production and monitoring errors. 
+              Write SQL scripts to support testing.
+	              </p>
+                <p>
+                <strong>Tech stack: C# ♦ .Net ♦ Selenium Webdriver ♦ Xunit ♦ Specflow ♦ React ♦ SQL Server ♦ Jenkins ♦ Teamcity ♦ Jira ♦ Agile ♦ 
+                  Kanban ♦ Gherkin ♦ Splunk ♦ POM modelling ♦ Xpath ♦ CSS ♦ Postman ♦ Powershell ♦ ReadyAPI</strong>
+                </p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">March 2018 - October 2019</span>
@@ -154,66 +104,24 @@ const IndexPage = () => (
             <div className="resume-content">
               <h3 className="mb-0">Test Analyst</h3>
               <div className="subheading mb-3">Linedata</div>
-              <p>Main responsibilities:</p>
-              <ul>
-                <li>
-                  Mantain the company's automation test suite and add new tests
-                  to it using HP UFT
-                </li>
-                <li>Raise defects in internal Defect Tracking System</li>
-                <li>
-                  Perform complex tests for QA stories and change requests
-                </li>
-                <li>Write scripts in PL/SQL</li>
-                <li>
-                  Create test plans based on specification and other QA
-                  documentation
-                </li>
-                <li>
-                  Work in SCRUM teams using AGILE methodology and work closely
-                  with development team and business analysts
-                </li>
-                <li>
-                  Work with Windows services and Build electronic trading test
-                  environments
-                </li>
-                <li>
-                  Test real-time data using Bloomberg Terminal and Web Services
-                </li>
-                <li>Work with Remote servers to resolve issues</li>
-                <li>Deployment of new builds in test environments</li>
-              </ul>
+              <p>
+              Perform complex tests for Financial asset management platform. Maintain the company's 
+              automation test suite and add new tests to it using HP UFT. 
+              Create test plans based on the specification and other QA documentation. Work in Scrum teams
+               using and work closely with the development team and business analysts. 
+              Work with Windows services and real-time data using Bloomberg Terminal and Web Services. 
+              Configure remote servers and deploy new builds
+	              </p>
+                <p>
+                <strong>Tech stack: FinTech ♦ Bloomberg ♦ Asset Management ♦ PL/SQL ♦ HP UFT ♦ Windows Services ♦ 
+                  Web Services ♦ Remote servers ♦ Tortoise Svn ♦ Agile ♦ Scrum</strong>
+                </p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">July 2015 - July 2017</span>
             </div>
           </div>
 
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div className="resume-content">
-              <h3 className="mb-0">Junior System Analyst</h3>
-              <div className="subheading mb-3">Exigen Services</div>
-              <p>Main responsibilities:</p>
-              <ul>
-                <li>
-                  Revise Product Design Specification for tax information system
-                </li>
-                <li>
-                  Compare GUI results with records in database using SQL
-                  statements
-                </li>
-                <li>Log defects in JIRA</li>
-                <li>
-                  Revise outdated test cases according to newest versions of use
-                  cases and new requirements
-                </li>
-                <li>Manually test recent changes</li>
-              </ul>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">May 2015 - Jun 2015</span>
-            </div>
-          </div>
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
             <div className="resume-content">
